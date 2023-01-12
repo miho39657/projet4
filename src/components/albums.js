1df887e2e5744f1f5ec8fd89b78d3bbe83bb78f9
zarhoul <zarhoul21@gmail.com>
2023-01-12 13:03:12 +0100

    Mod albums component !

--- src/components/albums.js
+++ src/components/albums.js
@@ -4,11 +4,10 @@ import axios from 'axios';
 function Albums() {
   const [albums,setAlbums]= useState([]);
   useEffect(() => {axios.get("https://jsonplaceholder.typicode.com/albums").then(res => setAlbums(res.data))},[]);
-  console.log(users);
-
+  
   return (
     <>
-    {users.map((elem,index) => <h1 key={index}> {elem.title}  </h1> )}
+    {albums.map((elem,index) => <h1 key={index}> {elem.title}  </h1> )}
     </>
   )
 }

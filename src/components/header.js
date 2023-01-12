87c0120230a6d86e55ce59069a9e33279478188b
zarhoul <zarhoul21@gmail.com>
2023-01-12 13:28:36 +0100

    First time add bootstrap !

--- src/components/header.js
+++ src/components/header.js
@@ -5,20 +5,20 @@ import { Outlet, Link } from "react-router-dom";
 const Header = () => {
     return (
         <>
-        <nav>
-            <ul>
-            <li>
-                <Link to="/users">Users</Link>
-            </li>
-            <li>
-                <Link to="/albums">Albums</Link>
-            </li>
-            <li>
-                <Link to="/posts">Posts</Link>
-            </li>
-            <li>
-                <Link to="/todos">Todos</Link>
-            </li>
+        <nav className="navbar navbar-expand-lg bg-light">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                    <Link className="navbar-brand" to="/users">Users</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="navbar-brand" to="/albums">Albums</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="navbar-brand" to="/posts">Posts</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="navbar-brand" to="/todos">Todos</Link>
+                </li>
             </ul>
         </nav>
 

ec941e8a6cadacb6b72422587af65673de12bd6a
zarhoul <zarhoul21@gmail.com>
2023-01-12 17:41:16 +0100

    Add style to all components !

--- src/components/todos.js
+++ src/components/todos.js
@@ -7,7 +7,18 @@ function Todos() {
 
   return (
     <>
-    {todos.map((elem,index) => <div key={index}> <h1>{elem.title} {elem.completed? <span>done</span> : ""}</h1>  </div> )}
+    <div className='row row-cols-1 row-cols-md-2 g-4'>
+        {todos.map((elem,index) =>  
+          <div className='col'>
+            <div className="card h-100 w-100">
+              <div className="card-body" key={index}>
+                <h5 className="card-title">{elem.title}</h5>
+                {elem.completed? <h4><span className="badge bg-success">Done</span></h4> : <h4><span className="badge bg-warning">Wanting</span></h4>}
+              </div>
+            </div>
+          </div> 
+        )}
+    </div>
     </>
   )
 }
